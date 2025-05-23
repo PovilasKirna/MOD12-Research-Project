1f1330b3d89135c1bd3164958e5ba392774e7315
Povilas Kirna <povilas.kirna@gmail.com>
2025-05-23 15:14:05 +0200

    chore: clean the dust2 filenames

--- research_project/src/utils/directory_files.py
+++ research_project/src/utils/directory_files.py
@@ -7,29 +7,29 @@ from typing import List
 
 def get_all_files_in_directory(directory: str) -> List[str]:
     """
-    Get all files in a directory and its subdirectories.
+    Get all files in a directory.
 
     Args:
         directory (str): The path to the directory.
 
     Returns:
-        List[str]: A list of file paths.
+        List[str]: A list of filenames.
     """
-    file_paths = []
+    file_names = []
     for root, _, files in os.walk(directory):
         for file in files:
-            file_paths.append(os.path.join(root, file))
-    return file_paths
+            file_names.append(file)
+    return file_names
 
 
-def export_file_paths_to_json(file_paths: List[str], output_file: str) -> None:
+def export_file_names_to_json(file_names: List[str], output_file: str) -> None:
     """
-    Export a list of file paths to a JSON file.
+    Export a list of filenames to a JSON file.
     Args:
-        file_paths (List[dict { "filename": str, "path": str }]): The list of file paths.
+        file_names (List[str]): The list of file names.
         output_file (str): The path to the output JSON file.
     """
-    data = [{"filename": os.path.basename(path), "path": path} for path in file_paths]
+    data = [{"filename": name} for name in file_names]
     with open(output_file, "w") as f:
         json.dump(data, f, indent=4)
 
@@ -38,9 +38,9 @@ def main():
     directory = "/Users/home/Desktop/University/MOD12/git/research-project/research_project/demos/.dust2_unlisted_demos"  # Replace with your directory path
     output_file = "file_paths.json"  # Replace with your desired output file name
 
-    file_paths = get_all_files_in_directory(directory)
-    export_file_paths_to_json(file_paths, output_file)
-    print(f"Exported {len(file_paths)} file paths to {output_file}")
+    file_names = get_all_files_in_directory(directory)
+    export_file_names_to_json(file_names, output_file)
+    print(f"Exported {len(file_names)} file names to {output_file}")
 
 
 if __name__ == "__main__":
